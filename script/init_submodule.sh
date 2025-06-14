a192e4e68a32e49dd8a2792771e70fc4c4961087
Juan Manuel Fernández Muñoz <jmfermun@gmail.com>
2025-06-14 21:03:19 +0200

    Added init submodules script. It checkouts the correct branches of each submodule.

---
+++ script/init_submodule.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Exit immediately if a command exits with a non-zero status.
+set -e
+
+# Change to respository root directory.
+cd "$(dirname "$0")"/..
+
+# Initialize the repository submodules.
+git submodule update --init --force --remote --recursive
+
+# Checkout the correct release of crosstool-NG.
+cd crosstool-ng
+git checkout crosstool-ng-1.27.0
+
+# Checkout the correct branch of Mastering Embedded Linux Programming Third Edition.
+cd ../melp
+git checkout master
+
+# Checkout the correct branch of U-Boot.
+cd ../u-boot
+git checkout v2025.04
